2f2a03f3501b01b6a2ed788b2b977a3d3b6118d2
Flamenrage <flamenrage@gmail.com>
2020-03-24 21:47:07 +0400

    lab 4 making

---
+++ IceCreamShopView/FormReportIceCreamIngredients.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace IceCreamShopView
+{
+    partial class FormReportIceCreamIngredients
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Ingredient = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IceCream = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonSaveToExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Ingredient,
+            this.IceCream,
+            this.Count});
+            this.dataGridView.GridColor = System.Drawing.SystemColors.ButtonShadow;
+            this.dataGridView.Location = new System.Drawing.Point(11, 64);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 28;
+            this.dataGridView.Size = new System.Drawing.Size(698, 502);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // Ingredient
+            // 
+            this.Ingredient.HeaderText = "Ингредиент";
+            this.Ingredient.MinimumWidth = 6;
+            this.Ingredient.Name = "Ingredient";
+            // 
+            // IceCream
+            // 
+            this.IceCream.HeaderText = "Мороженое";
+            this.IceCream.MinimumWidth = 6;
+            this.IceCream.Name = "IceCream";
+            // 
+            // Count
+            // 
+            this.Count.HeaderText = "Количество";
+            this.Count.MinimumWidth = 6;
+            this.Count.Name = "Count";
+            // 
+            // buttonSaveToExcel
+            // 
+            this.buttonSaveToExcel.Location = new System.Drawing.Point(11, 18);
+            this.buttonSaveToExcel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonSaveToExcel.Name = "buttonSaveToExcel";
+            this.buttonSaveToExcel.Size = new System.Drawing.Size(164, 33);
+            this.buttonSaveToExcel.TabIndex = 1;
+            this.buttonSaveToExcel.Text = "Сохранить в Excel";
+            this.buttonSaveToExcel.UseVisualStyleBackColor = true;
+            this.buttonSaveToExcel.Click += new System.EventHandler(this.buttonSaveToExcel_Click);
+            // 
+            // FormReportIceCreamIngredients
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 568);
+            this.Controls.Add(this.buttonSaveToExcel);
+            this.Controls.Add(this.dataGridView);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "FormReportIceCreamIngredients";
+            this.Text = "Ингредиенты по мороженому";
+            this.Load += new System.EventHandler(this.FormReportIceCreamIngredients_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ingredient;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IceCream;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Count;
+        private System.Windows.Forms.Button buttonSaveToExcel;
+    }
+}
---
+++ IceCreamShopView/FormReportIceCreamIngredients.cs
@@ -0,0 +1,70 @@
+﻿using IceCreamShopServiceDAL.BindingModels;
+using IceCreamShopServiceDAL.ServicesDal;
+using System;
+using System.Windows.Forms;
+using Unity;
+
+
+namespace IceCreamShopView
+{
+    public partial class FormReportIceCreamIngredients : Form
+    {
+        [Dependency]
+        public new IUnityContainer Container { get; set; }
+        private readonly ReportLogic logic;
+        public FormReportIceCreamIngredients(ReportLogic logic)
+        {
+            InitializeComponent();
+            this.logic = logic;
+        }
+        private void FormReportIceCreamIngredients_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var dict = logic.GetIceCreamIngredient();
+                if (dict != null)
+                {
+                    dataGridView.Rows.Clear();
+                    foreach (var elem in dict)
+                    {
+                        dataGridView.Rows.Add(new object[] { elem.IngredientName, "", "" });
+                        foreach (var listElem in elem.IceCreams)
+                        {
+                            dataGridView.Rows.Add(new object[] { "", listElem.Item1, listElem.Item2 });
+                        }
+                        dataGridView.Rows.Add(new object[] { "Итого", "", elem.TotalCount });
+                        dataGridView.Rows.Add(new object[] { });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+            }
+        }
+        private void buttonSaveToExcel_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new SaveFileDialog { Filter = "xlsx|*.xlsx" })
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        logic.SaveIceCreamIngredientToExcelFile(new ReportBindingModel
+                        {
+                            FileName = dialog.FileName
+                        });
+                        MessageBox.Show("Выполнено", "Успех", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+                       MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
