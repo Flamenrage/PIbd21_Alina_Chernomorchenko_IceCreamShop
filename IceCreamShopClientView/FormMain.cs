1b63f9f0aea93a9a447fc3f3c6cdf1b0bd95a3d6
Flamenrage <flamenrage@gmail.com>
2020-04-17 16:41:29 +0400

    lab 5 make

---
+++ IceCreamShopClientView/FormMain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IceCreamShopClientView
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            LoadList();
+        }
+
+        private void LoadList()
+        {
+            try
+            {
+                //dataGridViewClientOrders.DataSource = //прописать запрос;
+                dataGridViewClientOrders.Columns[0].Visible = false;
+                dataGridViewClientOrders.Columns[1].Visible = false;
+                dataGridViewClientOrders.Columns[2].Visible = false;
+                dataGridViewClientOrders.Columns[3].Visible = false;
+                dataGridViewClientOrders.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+            }
+        }
+
+        private void UpdateDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           /* var form = new FormUpdateData();
+            form.ShowDialog();*/
+        }
+
+        private void CreateOrderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+         /*   var form = new FormCreateOrder();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                LoadList();
+            }*/
+        }
+
+        private void RefreshOrderListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadList();
+        }
+    }
+}
