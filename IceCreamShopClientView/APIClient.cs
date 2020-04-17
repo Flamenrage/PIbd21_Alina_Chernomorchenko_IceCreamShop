625b587fc04f3aa0e2f91fb7e70db00802c2dd42
Flamenrage <flamenrage@gmail.com>
2020-04-17 16:11:46 +0400

    lab 5 make

---
+++ IceCreamShopClientView/APIClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamShopClientView
+{
+    class ApiClient
+    {
+    }
+}
---
+++ IceCreamShopRestApi/Controllers/MainController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using IceCreamShopServiceDAL.BindingModels;
+using IceCreamShopServiceDAL.ServicesDal;
+using IceCreamShopServiceDAL.Interfaces;
+using IceCreamShopServiceDAL.ViewModels;
+using IceCreamShopRestApi.Models;
+
+
+
+namespace IceCreamShopRestApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        private readonly IBookingService _order;
+        private readonly IIceCreamService _icecream;
+        private readonly MainService _main;
+
+        public MainController(IBookingService order, IIceCreamService product, MainService main)
+        {
+            _order = order;
+            _icecream = product;
+            _main = main;
+        }
+
+        [HttpGet]
+        public List<IceCream> GetIceCreams() => _icecream.Read(null)?.Select(rec => Convert(rec)).ToList();
+
+        [HttpGet]
+        public IceCream GetIceCream(int IceCreamId) => Convert(_icecream.Read(new IceCreamBindingModel
+        {
+            Id = IceCreamId
+        })?[0]);
+
+        [HttpGet]
+        public List<BookingViewModel> GetOrders(int clientId) => _order.Read(new BookingBindingModel
+        {
+            ClientId = clientId
+        });
+
+        [HttpPost]
+        public void CreateOrder(CreateBookingBindingModel model) => _main.CreateBooking(model);
+
+        private IceCream Convert(IceCreamViewModel model)
+        {
+            if (model == null) return null;
+            return new IceCream
+            {
+                Id = model.Id,
+                IceCreamName = model.IceCreamName,
+                Price = model.Price
+            };
+        }
+    }
+}
